5576f4afc654bd3d2bcfe7ab366148e9ba60cb59
upshkin <upshkin@gmail.com>
2017-10-23 17:58:29 +0300

    Added bootstrap framework support in templates and settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other apps
+    'bootstrap3',
     # My apps
     'blog.apps.BlogConfig',
 ]
